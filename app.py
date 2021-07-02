d58113b378cccf0e368692c4d1c5e130bf7f1d16
jtb117 <jtb117@pitt.edu>
2021-07-02 11:35:17 -0400

    Fix typo in table_exists

--- app.py
+++ app.py
@@ -148,7 +148,7 @@ def remember(data):
         trig  = command[0]
         response = command[1]
         old_trig = trig
-        if not data_access.pr_table_exists():
+        if not data_access.table_exists():
             data_access.execute_query(DB_QUERIES["CREATE_PR_TABLE"])
         if "'" in trig:
             start = trig.find("'")
