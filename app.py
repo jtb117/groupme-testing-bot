311a014fbf4d1c4ef0ff4b60c52234c82f814b61
James <jtb117@pitt.edu>
2022-07-18 20:02:58 -0600

    Remove answer

--- app.py
+++ app.py
@@ -383,8 +383,7 @@ def _openai(text):
             max_tokens= 2000,
         )
     output = response['choices'][0]['text'] #Lol
-    if output.strip() == '':
-        output = "Something went wrong. Can't help you."
+    output = output.replace("Fellasbot: ", "")
     _log(f'ai output: {output}')
     return output.strip()
 
